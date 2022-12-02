fe4af7ba2a89966289e3a8577e379bdfaf221247
AlexeiTim <timashkov060896@gmail.com>
2022-12-02 18:31:02 +0300

    init:add project

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Body, Post, HttpCode } from '@nestjs/common';
+import { CreateUserDTO } from '../user/dto';
+import { AuthService } from './auth.service';
+import { UserLoginDTO } from './dto';
+import { AuthUserResponse } from './response';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+import { UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/guards/jwt-guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authSerive: AuthService) { }
+
+  @ApiTags('API')
+  @ApiResponse({ status: 201, type: CreateUserDTO })
+  @HttpCode(201)
+  @Post('register')
+  register(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
+    return this.authSerive.registerUser(dto)
+  }
+
+  
+  @ApiTags('API')
+  @ApiResponse({ status: 200, type: AuthUserResponse })
+  @HttpCode(200)
+  @Post('login')
+  login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
+    return this.authSerive.loginUser(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('test')
+  test() {
+    return true
+  }
+
+}
